58f35609b14c26e39544870c61595db96d4bfb70
Satendra Singh <satendrasingh1325@gmail.com>
2023-09-23 13:46:44 +0530

    Done Job Details Page. Now we Implement Search fun

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,12 +1,11 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 
 import styles from "./nearbyjobcard.style";
-
 import { checkImageURL } from "../../../../utils";
 
 const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
-    <TouchableOpacity style={styles.container} onPress={() => handleNavigate}>
+    <TouchableOpacity style={styles.container} onPress={handleNavigate}>
       <TouchableOpacity style={styles.logoContainer}>
         <Image
           source={{
@@ -18,18 +17,14 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
           style={styles.logoImage}
         />
       </TouchableOpacity>
+
       <View style={styles.textContainer}>
         <Text style={styles.jobName} numberOfLines={1}>
-          {job.job_title}
+          {job?.job_title}
         </Text>
-        <Text style={styles.location}>{job.job_employment_type}</Text>
+
+        <Text style={styles.jobType}>{job?.job_employment_type}</Text>
       </View>
-      {/* <View style={styles.jobName}>
-        <Text style={styles.jobName} numberOfLines={1}>
-          {job.job_title}
-        </Text>
-        <Text style={styles.location}>{job.job_country}</Text>
-      </View> */}
     </TouchableOpacity>
   );
 };
